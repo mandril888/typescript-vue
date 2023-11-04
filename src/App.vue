b980c470d604f32b4e653c1e7da34a869e78ae98
Arturo Rubio <arubio@seocom.agency>
2023-11-04 22:31:20 +0100

    create User interface

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
+import type User from "./types/User";
+import { reactive } from "vue";
+
+const user: User = reactive({
+  id: 1,
+  name: "mandril888",
+  settings: [],
+});
+
+console.log(user.id);
 </script>
 
 <template>
